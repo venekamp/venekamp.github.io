1a7b0b74ab23f7488160530af80f0cbb15519977
Gerben Venekamp <venekamp@gmail.com>
2016-12-18 21:51:49 +0100

    Add functionality (javascript/jquery)

---
+++ javascripts/keepabove.js
@@ -0,0 +1,82 @@
+(function($) {
+    $.keepAboveElement = function(el, options) {
+        this.$el = $(el);
+        this.el = el;
+
+        var element = this.$el;
+
+        var elementHeight = element.height();
+        var tripPoint = options.tripPoint;
+
+        console.log("tripPoint = " + tripPoint + "  elementHeight = " + elementHeight);
+
+        function setValues() {
+            elementHeight = element.height();
+            console.log("elementHeight = " + elementHeight);
+        };
+
+        function checkTripValue() {
+            var currentOffset = window.pageYOffset + window.innerHeight;
+
+            if (currentOffset < tripPoint) {
+                setToFixedOnceOnly(0);
+            } else {
+                setToAbsoluteOnceOnly(tripPoint - elementHeight);
+            }
+        };
+
+        function setToFixedOnceOnly(offset) {
+            if (element.css("position") != "fixed") {
+                setToFixed(0);
+            }
+        };
+
+        function setToFixed(offset) {
+            var cssOptions = {
+                "position": "fixed",
+                "top": "",
+                "bottom": "0",
+                "height": elementHeight + "px"
+            }
+
+            element.css(cssOptions);
+            console.log("SetToFixed:\n" + JSON.stringify(cssOptions, null, 4));
+        };
+
+        function setToAbsoluteOnceOnly(offset) {
+            if (element.css("position") != "absolute") {
+                setToAbsolute(offset);
+            }
+        };
+
+        function setToAbsolute(offset) {
+            var cssOptions = {
+                "position": "absolute",
+                "top": offset + "px",
+                "bottom": "",
+                "height": elementHeight + "px"
+            };
+
+            element.css(cssOptions);
+            console.log("SetToAbsolute:\n" + JSON.stringify(cssOptions, null, 4));
+        };
+
+        this.init = function() {
+            $(document).ready(function() { checkTripValue(); });
+
+            $(window).on("scroll", checkTripValue);
+
+            // TODO: Resizeing the window should also recaulate the tripPoint.
+            $(window).on("resize", checkTripValue);
+        };
+
+        this.init();
+    };
+
+    $.fn.keepAboveElement = function(options) {
+        return this.each(function() {
+            (new $.keepAboveElement(this, options));
+        });
+    };
+
+})(jQuery);
